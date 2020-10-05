8842853a8c80b1403a5413f186121a07fb8cbd32
hazemsakr <70215083+hazemsakr@users.noreply.github.com>
2020-10-05 03:44:42 +0200

    Update Update-All.py

--- Update-All.py
+++ Update-All.py
@@ -1,6 +1,7 @@
 import os
 os.system("clear")
-os.system("apt update && apt upgrade -y")
+os.system("apt update")
+os.system("apt upgrade -y")
 os.system("pkg install git")
 os.system("clear")
 print(:ALL UPDATES-DONE")
